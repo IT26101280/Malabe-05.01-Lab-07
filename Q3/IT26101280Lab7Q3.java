fac9ffc4a2e242c49294b85f3aabf1597f83da49
IT26101280 <it26101280@my.sliit.lk>
2026-09-15 20:06:17 +0530

    IT26101280Lab7Q3.java

---
+++ Q3/IT26101280Lab7Q3.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class IT26101280Lab7Q3{
+	public static void main(String[] args) {
+	
+	final double DISCOUNT_RATE = 0.05;
+	final int NUMBER_OF_CUSTOMERS = 5;
+	
+	double billAmount, amountToPay;
+	char paymentMode;
+	
+	double discount = 0;
+	
+	Scanner input = new Scanner(System.in);
+	for (int customerCount = 1; customerCount <= NUMBER_OF_CUSTOMERS; customerCount++) {
+		
+		System.out.println("Customer" + customerCount);
+		
+		System.out.print("Enter total bill amount: ");
+		billAmount = input.nextDouble();
+		
+		System.out.print("Enter mode of payment ( C for cash, O for other): ");
+		paymentMode = input.next().charAt(0);
+		
+		if (paymentMode == 'C' ||paymentMode == 'c') {
+			
+			discount = billAmount * DISCOUNT_RATE;
+			amountToPay = billAmount - discount;
+			
+			System.out.println("Discount is : " + discount);
+			System.out.println("Amount to be paid: " + amountToPay);
+		}
+		else if ( paymentMode == 'O' || paymentMode == 'o'){
+			amountToPay = billAmount;
+			
+			System.out.println("No discount applicable");
+			System.out.println("Amount to be paid: " + amountToPay);
+		}
+		else{
+			System.out.println("Payment Mode is Not Valid");
+			System.out.println();
+			
+			continue;
+		}
+		System.out.println();
+	}
+	}
+}
+			
+			
+	
